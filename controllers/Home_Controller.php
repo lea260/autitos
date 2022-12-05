a451e36de26888e917d3cd127ac53ac9a9360a94
Leandro <leandro@mail.com>
2022-12-05 18:56:57 -0300

    hora local

---
+++ controllers/Home_Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+class Home_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function hora()
+    {
+        $date = new DateTime('2000-12-17 23:13:10', new DateTimeZone('America/Montevideo'));
+        //echo $date->format('Y-m-d H:i:s') . "\n";
+        echo "\n";
+        $date->setTimezone(new DateTimeZone('America/Montevideo'));
+        echo $date->format('Y-m-d H:i:s') . "\n";
+
+    }
+    public function hora2()
+    {
+        $datetime = date("Y-m-d H:i:s");
+        $utc      = new DateTime($datetime, new DateTimeZone('UTC'));
+        $utc->setTimezone(new DateTimeZone('America/Montevideo'));
+        echo $utc->format('Y-m-d H:i:s');
+
+    }
+}
